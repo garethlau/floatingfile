16397a2bc57489e85ee515b06698974eee2e02f9
Gareth <Gareth.kh.lau@gmail.com>
2020-12-24 10:36:40 -0700

    Register v4 APIs

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,28 @@ const router = express.Router();
 const middlewares = require("../middleware");
 
 let siteAssociation = {
-  applinks: {
-    apps: [],
-    details: [
-      {
-        appID: "8VU5ULQ45F.com.alanyan.Floating-File",
-        paths: [
-          "NOT /privacy/*",
-          "NOT /terms/*",
-          "NOT /donate/*",
-          "NOT /support/*",
-          "/*",
-        ],
-      },
-    ],
-  },
+	applinks: {
+		apps: [],
+		details: [
+			{
+				appID: "8VU5ULQ45F.com.alanyan.Floating-File",
+				paths: ["NOT /privacy/*", "NOT /terms/*", "NOT /donate/*", "NOT /support/*", "/*"],
+			},
+		],
+	},
 };
 
 router.get("/.well-known/apple-app-site-association", (req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(siteAssociation));
+	res.writeHead(200, { "Content-Type": "application/json" });
+	res.end(JSON.stringify(siteAssociation));
 });
 
 router.get("/apple-app-site-association", (req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(siteAssociation));
+	res.writeHead(200, { "Content-Type": "application/json" });
+	res.end(JSON.stringify(siteAssociation));
 });
 
 router.use("/api/v3", middlewares.requireKey, require("./v3"));
+router.use("/api/v4", middlewares.requireKey, require("./v4"));
 
 module.exports = router;
