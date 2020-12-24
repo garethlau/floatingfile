426dba241c1c1af75a65cf107106cf8ccf818748
Gareth <Gareth.kh.lau@gmail.com>
2020-12-24 15:17:07 -0700

    Config AWS with new env variables

--- s3/index.js
+++ s3/index.js
@@ -1,10 +1,12 @@
 const AWS = require("aws-sdk");
 const keys = require("../config/keys");
 
-const s3 = new AWS.S3({
-	accessKeyId: keys.s3AccessKeyId,
-	secretAccessKey: keys.s3AccessKeySecret,
-	Bucket: keys.s3BucketName,
+AWS.config.update({
+	accessKeyId: keys.AWS_ACCESS_KEY_ID,
+	secretAccessKey: keys.AWS_ACCESS_KEY_SECRET,
+	signatureVersion: "v4",
+	region: "us-east-2",
 });
 
+const s3 = new AWS.S3();
 module.exports = s3;
