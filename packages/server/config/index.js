14149470cd264af42fcff5bc7793c447f8c16fd2
Gareth <Gareth.kh.lau@gmail.com>
2021-02-19 16:02:24 -0700

    refactor: Use dotenv to handle env variables

---
+++ packages/server/config/index.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+  PORT: process.env.PORT || 5000,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGO_URI: process.env.MONGO_URI,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_ACCESS_KEY_SECRET: process.env.AWS_ACCESS_KEY_SECRET,
+  S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+};
