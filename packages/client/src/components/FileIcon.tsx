403c473e49b9d76f8b57dea62f3b5e543779d311
Gareth <Gareth.kh.lau@gmail.com>
2021-03-04 14:17:13 -0700

    refactor: Create FileIcon component

---
+++ packages/client/src/components/FileIcon.tsx
@@ -0,0 +1,150 @@
+import React from "react";
+import {
+  mdiMicrosoftWord,
+  mdiMicrosoftPowerpoint,
+  mdiMicrosoftExcel,
+  mdiImageOutline,
+  mdiCodeJson,
+  mdiLanguageJavascript,
+  mdiLanguageHtml5,
+  mdiLanguagePython,
+  mdiFilePdfOutline,
+  mdiFileDocumentOutline,
+  mdiFileQuestionOutline,
+  mdiFolderZipOutline,
+  mdiSvg,
+  mdiLanguageSwift,
+  mdiLanguageCss3,
+  mdiGif,
+  mdiLanguageC,
+  mdiLanguageCpp,
+  mdiLanguageCsharp,
+  mdiLanguageGo,
+  mdiLanguageFortran,
+  mdiLanguageHaskell,
+  mdiLanguageTypescript,
+  mdiLanguageJava,
+  mdiLanguageKotlin,
+  mdiLanguageMarkdown,
+  mdiLanguagePhp,
+  mdiLanguageR,
+  mdiLanguageRuby,
+  mdiLanguageXaml,
+} from "@mdi/js";
+import Icon from "@mdi/react";
+
+function getPath(extension: string) {
+  let path = mdiFileQuestionOutline;
+  switch (extension) {
+    case "c":
+      path = mdiLanguageC;
+      break;
+    case "cpp":
+      path = mdiLanguageCpp;
+      break;
+    case "cs":
+      path = mdiLanguageCsharp;
+      break;
+    case "go":
+      path = mdiLanguageGo;
+      break;
+    case "ts":
+      path = mdiLanguageTypescript;
+      break;
+    case "java":
+      path = mdiLanguageJava;
+      break;
+    case "rb":
+      path = mdiLanguageRuby;
+      break;
+    case "xml":
+      path = mdiLanguageXaml;
+      break;
+    case "md":
+      path = mdiLanguageMarkdown;
+      break;
+    case "kt":
+    case "kts":
+    case "ktm":
+      path = mdiLanguageKotlin;
+      break;
+    case "php":
+      path = mdiLanguagePhp;
+      break;
+    case "hs":
+      path = mdiLanguageHaskell;
+      break;
+    case "r":
+      path = mdiLanguageR;
+      break;
+
+    case "svg":
+      path = mdiSvg;
+      break;
+    case "png":
+    case "ico":
+    case "jpg":
+    case "jpeg":
+      path = mdiImageOutline;
+      break;
+    case "json":
+      path = mdiCodeJson;
+      break;
+    case "pdf":
+      path = mdiFilePdfOutline;
+      break;
+    case "txt":
+      path = mdiFileDocumentOutline;
+      break;
+    case "xlsx":
+    case "xls":
+      path = mdiMicrosoftExcel;
+      break;
+    case "docx":
+    case "doc":
+      path = mdiMicrosoftWord;
+      break;
+    case "pptx":
+    case "ppt":
+      path = mdiMicrosoftPowerpoint;
+      break;
+    case "mp4":
+      break;
+    case "js":
+      path = mdiLanguageJavascript;
+      break;
+    case "py":
+      path = mdiLanguagePython;
+      break;
+    case "html":
+      path = mdiLanguageHtml5;
+      break;
+    case "zip":
+      path = mdiFolderZipOutline;
+      break;
+    case "swift":
+      path = mdiLanguageSwift;
+      break;
+    case "css":
+      path = mdiLanguageCss3;
+      break;
+    case "gif":
+      path = mdiGif;
+      break;
+    default:
+      path = mdiFileQuestionOutline;
+      break;
+  }
+  return path;
+}
+const FileIcon: React.FC<{ extension: string }> = ({ extension }) => {
+  return (
+    <Icon
+      path={getPath(extension)}
+      size="42px"
+      style={{ marginTop: "5px", opacity: 0.75 }}
+    />
+  );
+};
+
+export default FileIcon;
--- packages/client/src/components/FileListItem.tsx
+++ packages/client/src/components/FileListItem.tsx
@@ -8,39 +8,6 @@ import useWindowWidth from "../hooks/useWindowWidth";
 import CloudDownloadIcon from "@material-ui/icons/CloudDownload";
 import DeleteIcon from "@material-ui/icons/Delete";
 import OpenInBrowserIcon from "@material-ui/icons/OpenInBrowser";
-import {
-  mdiMicrosoftWord,
-  mdiMicrosoftPowerpoint,
-  mdiMicrosoftExcel,
-  mdiImageOutline,
-  mdiCodeJson,
-  mdiLanguageJavascript,
-  mdiLanguageHtml5,
-  mdiLanguagePython,
-  mdiFilePdfOutline,
-  mdiFileDocumentOutline,
-  mdiFileQuestionOutline,
-  mdiFolderZipOutline,
-  mdiSvg,
-  mdiLanguageSwift,
-  mdiLanguageCss3,
-  mdiGif,
-  mdiLanguageC,
-  mdiLanguageCpp,
-  mdiLanguageCsharp,
-  mdiLanguageGo,
-  mdiLanguageFortran,
-  mdiLanguageHaskell,
-  mdiLanguageTypescript,
-  mdiLanguageJava,
-  mdiLanguageKotlin,
-  mdiLanguageMarkdown,
-  mdiLanguagePhp,
-  mdiLanguageR,
-  mdiLanguageRuby,
-  mdiLanguageXaml,
-} from "@mdi/js";
-import Icon from "@mdi/react";
 import { isMobile } from "react-device-detect";
 import useRemoveFile from "../mutations/useRemoveFile";
 import { useParams } from "react-router-dom";
@@ -49,6 +16,7 @@ import axios from "axios";
 import { saveBlob } from "../utils";
 import { BASE_API_URL } from "../env";
 import { File } from "@floatingfile/types";
+import FileIcon from "./FileIcon";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -143,117 +111,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function renderIcon(extension: string) {
-  let path = mdiFileQuestionOutline;
-  switch (extension) {
-    case "c":
-      path = mdiLanguageC;
-      break;
-    case "cpp":
-      path = mdiLanguageCpp;
-      break;
-    case "cs":
-      path = mdiLanguageCsharp;
-      break;
-    case "go":
-      path = mdiLanguageGo;
-      break;
-    case "ts":
-      path = mdiLanguageTypescript;
-      break;
-    case "java":
-      path = mdiLanguageJava;
-      break;
-    case "rb":
-      path = mdiLanguageRuby;
-      break;
-    case "xml":
-      path = mdiLanguageXaml;
-      break;
-    case "md":
-      path = mdiLanguageMarkdown;
-      break;
-    case "kt":
-    case "kts":
-    case "ktm":
-      path = mdiLanguageKotlin;
-      break;
-    case "php":
-      path = mdiLanguagePhp;
-      break;
-    case "hs":
-      path = mdiLanguageHaskell;
-      break;
-    case "r":
-      path = mdiLanguageR;
-      break;
-
-    case "svg":
-      path = mdiSvg;
-      break;
-    case "png":
-    case "ico":
-    case "jpg":
-    case "jpeg":
-      path = mdiImageOutline;
-      break;
-    case "json":
-      path = mdiCodeJson;
-      break;
-    case "pdf":
-      path = mdiFilePdfOutline;
-      break;
-    case "txt":
-      path = mdiFileDocumentOutline;
-      break;
-    case "xlsx":
-    case "xls":
-      path = mdiMicrosoftExcel;
-      break;
-    case "docx":
-    case "doc":
-      path = mdiMicrosoftWord;
-      break;
-    case "pptx":
-    case "ppt":
-      path = mdiMicrosoftPowerpoint;
-      break;
-    case "mp4":
-      break;
-    case "js":
-      path = mdiLanguageJavascript;
-      break;
-    case "py":
-      path = mdiLanguagePython;
-      break;
-    case "html":
-      path = mdiLanguageHtml5;
-      break;
-    case "zip":
-      path = mdiFolderZipOutline;
-      break;
-    case "swift":
-      path = mdiLanguageSwift;
-      break;
-    case "css":
-      path = mdiLanguageCss3;
-      break;
-    case "gif":
-      path = mdiGif;
-      break;
-    default:
-      path = mdiFileQuestionOutline;
-      break;
-  }
-  return (
-    <Icon
-      path={path}
-      size={"42px"}
-      style={{ marginTop: "5px", opacity: 0.75 }}
-    />
-  );
-}
-
 const FileListItem: React.FC<{ file: File }> = ({ file }) => {
   const { name, type, key, ext, size, signedUrl } = file;
   const { code }: { code: string } = useParams();
@@ -306,7 +163,9 @@ const FileListItem: React.FC<{ file: File }> = ({ file }) => {
       style={{ backgroundColor: isSelected(key) ? "#DDE8F8" : Colors.WHITE }}
     >
       <div className={cls.icon} onClick={() => toggleSelect(key)}>
-        <Center>{renderIcon(ext)}</Center>
+        <Center>
+          <FileIcon extension={ext} />
+        </Center>
       </div>
       <div
         style={{ marginTop: "10px", minWidth: 0 }}
