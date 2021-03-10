90b2b85661202f01461f90af2e107ed68e22a56d
Gareth <Gareth.kh.lau@gmail.com>
2021-03-10 09:14:09 -0700

    Refactor Footer component
    - Use Material-UI Grid component for future flexibility
    - Create new section for developer links

--- packages/landing/src/components/Footer.tsx
+++ packages/landing/src/components/Footer.tsx
@@ -1,38 +1,59 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "./Container";
-import Center from "./Center";
 import Link from "next/link";
 import { Colors } from "../constants";
+import Grid from "@material-ui/core/Grid";
 
-const exploreLinks = [
-  { text: "For Developers", path: "/docs/v4" },
-  { text: "Change Log", path: "/changelog" },
-  { text: "Frequently Asked Questions", path: "/faq" },
-  { text: "Download", path: "/download" },
-];
-const resourceLinks = [
-  { text: "Terms of Service", path: "/terms" },
-  { text: "Privacy Policy", path: "/privacy" },
-  // { text: "Cookie Policy", path: "/cookie" },
+const resources = [
+  {
+    heading: "Explore",
+    links: [
+      { text: "Changelog", path: "/changelog" },
+      { text: "Frequently Asked Questions", path: "/faq" },
+      { text: "Download", path: "/download" },
+    ],
+  },
+  {
+    heading: "Developers",
+    links: [
+      { text: "API Documentation", path: "/docs/v4" },
+      { text: "Repository", href: "https://github.com/garethlau/floatingfile" },
+    ],
+  },
+  {
+    heading: "Legal",
+    links: [
+      { text: "Terms of Service", path: "/terms" },
+      { text: "Privacy Policy", path: "/privacy" },
+    ],
+  },
 ];
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: Colors.LIGHT_SHADE,
+    padding: "20px",
+  },
+  brand: {
+    minHeight: "100px",
   },
   content: {
     display: "grid",
     gridTemplateRows: "auto auto auto",
     gridTemplateColumns: "1fr",
-    padding: "20px",
     minHeight: "175px",
     [theme.breakpoints.up("md")]: {
       gridTemplateRows: "1fr",
-      gridTemplateColumns: "1fr 250px 250px",
+      gridTemplateColumns: "1fr 250px 250px 250px",
     },
   },
-  groupTitle: {
+  icon: {
+    float: "left",
+    height: "48px",
+    marginRight: "20px",
+  },
+  heading: {
     color: Colors.PRIMARY,
     margin: 0,
     fontWeight: 600,
@@ -40,6 +61,9 @@ const useStyles = makeStyles((theme) => ({
   link: {
     margin: 0,
     opacity: 0.7,
+    display: "block",
+    textDecoration: "none",
+    color: "#000000",
     transition: "ease 0.3s",
     "&:hover": {
       cursor: "pointer",
@@ -49,43 +73,41 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Footer: React.FC<{}> = () => {
-  const cls = useStyles();
+  const classes = useStyles();
   return (
-    <footer className={cls.root}>
+    <footer className={classes.root}>
       <Container>
-        <div className={cls.content}>
-          <Center>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={6} className={classes.brand}>
             <img
               alt="floatingfile icon"
-              style={{ float: "left", height: "48px", marginRight: "20px" }}
+              className={classes.icon}
               src="/floatingfile.png"
             />
             <div style={{ textAlign: "left", float: "left" }}>
-              <h3 className={cls.groupTitle}>floatingfile</h3>
+              <h3 className={classes.heading}>floatingfile</h3>
               <p style={{ margin: "0" }}>File transfer, simplified.</p>
             </div>
-          </Center>
-          <div style={{ marginTop: "20px" }}>
-            <h3 className={cls.groupTitle}>Explore</h3>
-            {exploreLinks.map((link, index) => {
-              return (
-                <Link href={link.path} key={index}>
-                  <p className={cls.link}>{link.text}</p>
-                </Link>
-              );
-            })}
-          </div>
-          <div style={{ marginTop: "20px" }}>
-            <h3 className={cls.groupTitle}>Resources</h3>
-            {resourceLinks.map((link, index) => {
-              return (
-                <Link href={link.path} key={index}>
-                  <p className={cls.link}>{link.text}</p>
-                </Link>
-              );
-            })}
-          </div>
-        </div>
+          </Grid>
+          <Grid container spacing={3} item xs={12} md={6}>
+            {resources.map(({ heading, links }) => (
+              <Grid item md={6} sm={6} xs={12}>
+                <h3 className={classes.heading}>{heading}</h3>
+                {links.map(({ text, href, path }, index) =>
+                  path ? (
+                    <Link key={index} href={path}>
+                      <a className={classes.link}>{text}</a>
+                    </Link>
+                  ) : (
+                    <a key={index} href={href} className={classes.link}>
+                      {text}
+                    </a>
+                  )
+                )}
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
       </Container>
     </footer>
   );
