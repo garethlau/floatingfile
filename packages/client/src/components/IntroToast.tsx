acdc3cb26b6bd9142dd17d6dd29e7bc610557752
Gareth <Gareth.kh.lau@gmail.com>
2021-03-12 09:17:00 -0700

    feat(@floatingfile/client): Update intro message

--- packages/client/src/components/IntroToast.tsx
+++ packages/client/src/components/IntroToast.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Button from "./Button";
 import { USERNAME_STORAGE_KEY } from "../env";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    maxWidth: "400px",
+  },
+  button: {
+    marginRight: "5px",
+  },
+}));
 
 const IntroToast: React.FC<{
   handleClose: React.MouseEventHandler<HTMLButtonElement>;
 }> = ({ handleClose }) => {
+  const classes = useStyles();
   const username = localStorage.getItem(USERNAME_STORAGE_KEY || "");
 
   return (
-    <div style={{ maxWidth: "400px" }}>
+    <div className={classes.container}>
       <p>
         Welcome to floatingfile
         <span
@@ -20,10 +31,24 @@ const IntroToast: React.FC<{
         </span>
       </p>
       <p>
-        {`Your randomly generated nickname is: ${username} Please be aware that anyone can download your files long as they are in the space. Files are automatically deleted after 60 minutes.`}
+        Your randomly generated nickname is: <b>{username}</b>. Please be aware
+        that anyone can download your files long as they are in the space.
       </p>
-      <Button variant="primary" onClick={handleClose} debounce={5}>
+      <Button
+        className={classes.button}
+        variant="primary"
+        onClick={handleClose}
+        debounce={5}
+      >
         Got It
+      </Button>
+      <Button
+        className={classes.button}
+        variant="primary"
+        inverse
+        href="https://floatingfile.space/faq"
+      >
+        Learn More
       </Button>
     </div>
   );
