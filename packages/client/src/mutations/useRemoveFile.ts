e11f3ccd68519886365be3e43528a5c2d0f19aaf
Gareth <Gareth.kh.lau@gmail.com>
2021-03-16 08:15:13 -0600

    fix(@floatingfile/client): Optimisticly remove files

--- packages/client/src/mutations/useRemoveFile.ts
+++ packages/client/src/mutations/useRemoveFile.ts
@@ -1,40 +1,33 @@
 import { useMutation, useQueryClient } from "react-query";
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 import { BASE_API_URL } from "../env";
-import { File } from "@floatingfile/types";
+import { Space } from "@floatingfile/types";
 
 export default function useRemoveFile(code: string) {
   const queryClient = useQueryClient();
-  const queryKey = ["space", code, "files"];
+  const queryKey = ["space", code];
 
-  function mutate(key: string): Promise<AxiosResponse> {
-    return axios.delete(`${BASE_API_URL}/api/v5/spaces/${code}/files/${key}`);
-  }
-
-  async function onMutate(key: string) {
-    await queryClient.cancelQueries(queryKey);
-
-    const snapshot = queryClient.getQueryData(queryKey);
-
-    queryClient.setQueryData<Array<File>>(queryKey, (prev = []) => {
-      let newFiles = prev.filter((file) => file.key !== key);
-      return newFiles;
-    });
-
-    return { snapshot };
-  }
-
-  function onError(_0: any, _1: any, context: any) {
-    queryClient.setQueryData(queryKey, context.snapshot);
-  }
-
-  function onSettled() {
-    queryClient.invalidateQueries(queryKey);
-  }
-  const config = {
-    onMutate,
-    onError,
-    onSettled,
-  };
-  return useMutation(mutate, config);
+  return useMutation(
+    (key: string) =>
+      axios.delete(`${BASE_API_URL}/api/v5/spaces/${code}/files/${key}`),
+    {
+      onMutate: async (key: string) => {
+        await queryClient.cancelQueries(queryKey);
+        const snapshot = queryClient.getQueryData<Space>(queryKey);
+        queryClient.setQueryData<Space | undefined>(queryKey, (prev) => {
+          if (!prev) return;
+          let newFiles = prev.files.filter((file) => file.key !== key);
+          prev.files = newFiles;
+          return prev;
+        });
+        return snapshot;
+      },
+      onError: (_0, _1, context) => {
+        queryClient.setQueryData<Space | undefined>(queryKey, context);
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(queryKey);
+      },
+    }
+  );
 }
--- packages/client/src/mutations/useRemoveFiles.ts
+++ packages/client/src/mutations/useRemoveFiles.ts
@@ -1,58 +1,40 @@
 import { useMutation, useQueryClient } from "react-query";
 import { BASE_API_URL } from "../env";
 import axios from "axios";
-import { File } from "@floatingfile/types";
+import { Space } from "@floatingfile/types";
 
 export default function useRemoveFiles(code: string) {
   const queryClient = useQueryClient();
-  function mutate(keysToRemove: string[]) {
-    return new Promise((resolve, reject) => {
-      axios
-        .delete(
-          `${BASE_API_URL}/api/v5/spaces/${code}/files?toRemove=${JSON.stringify(
-            keysToRemove
-          )}`
-        )
-        .then((response) => {
-          resolve(response);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
-  }
+  const queryKey = ["space", code];
 
-  const queryKey = ["space", code, "files"];
-
-  async function onMutate(keysToRemove: string[]) {
-    await queryClient.cancelQueries(queryKey);
-
-    const snapshot = queryClient.getQueryData(queryKey);
-
-    queryClient.setQueryData<Array<File>>(queryKey, (prev = []) => {
-      let remainingFiles = prev.filter(
-        (file) => !keysToRemove.includes(file.key)
+  return useMutation(
+    (keysToRemove: string[]) => {
+      return axios.delete(
+        `${BASE_API_URL}/api/v5/spaces/${code}/files?toRemove=${JSON.stringify(
+          keysToRemove
+        )}`
       );
-      return remainingFiles;
-    });
-
-    return { snapshot };
-  }
-
-  function onError(_0: any, _1: any, context: any) {
-    queryClient.setQueryData(queryKey, context.snapshot);
-  }
-
-  function onSettled() {
-    queryClient.invalidateQueries(queryKey);
-    queryClient.invalidateQueries(["space", code]);
-  }
-
-  const config = {
-    onMutate,
-    onError,
-    onSettled,
-  };
-
-  return useMutation(mutate, config);
+    },
+    {
+      onMutate: async (toRemove: string[]) => {
+        await queryClient.cancelQueries(queryKey);
+        const snapshot = queryClient.getQueryData<Space>(queryKey);
+        queryClient.setQueryData<Space | undefined>(queryKey, (prev) => {
+          if (!prev) return;
+          const remainingFiles = prev.files.filter(
+            (file) => !toRemove.includes(file.key)
+          );
+          prev.files = remainingFiles;
+          return prev;
+        });
+        return snapshot;
+      },
+      onError: (_0, _1, context) => {
+        queryClient.setQueryData<Space | undefined>(queryKey, context);
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(queryKey);
+      },
+    }
+  );
 }
