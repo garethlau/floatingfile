2a44733ea73923a60d432313b34b898fffd17ea0
Gareth Lau <gareth.kh.lau@gmail.com>
2021-08-09 12:05:32 -0600

    set useCreateIndex to true

--- packages/server/src/db/index.ts
+++ packages/server/src/db/index.ts
@@ -5,7 +5,11 @@ export default function init(): Promise<null> {
   return new Promise((resolve, reject) => {
     mongoose.connect(
       MONGO_URI,
-      { useUnifiedTopology: true, useNewUrlParser: true },
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true
+      },
       (error) => {
         if (error) {
           reject(error);
