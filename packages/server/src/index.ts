eaf3dbd38f458422a6e54d6fa78a86d27806573f
Gareth Lau <gareth.kh.lau@gmail.com>
2021-08-09 11:40:44 -0600

    Update static file directory

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -17,10 +17,9 @@ import initDb from "./db";
     logger.info("ENVIRONMENT IS " + NODE_ENV);
     logger.info("SERVING CLIENT");
 
-    const APP_OUT_DIRECTORY = path.join(__dirname, "..", "..", "client/out");
+    const APP_OUT_DIRECTORY = path.join(__dirname, "..", "..", "..", "client", "out");
     const APP_INDEX = path.join(APP_OUT_DIRECTORY, "index.html");
 
-    app.use(express.static(path.join(__dirname, "..", "client", "build")));
     app.use(express.static(APP_OUT_DIRECTORY));
     app.get("*", (_, res: Response) => {
       res.sendFile(APP_INDEX);
