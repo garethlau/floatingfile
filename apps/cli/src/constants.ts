f417534cb6c4466e2f247c60cd68d79f663d9140
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-01-15 22:36:00 -0700

    Production release

--- apps/cli/src/constants.ts
+++ apps/cli/src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://staging.floatingfile.space";
-export const APP_URL = "https://staging.floatingfile.space";
+export const API_URL = "https://app.floatingfile.space";
+export const APP_URL = "https://app.floatingfile.space";
