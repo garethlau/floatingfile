0130fe053030e1e0d03fe31fad1e08d3f94d349b
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-01-15 17:44:26 -0700

    Update banner image url

--- apps/landing/pages/faq.tsx
+++ apps/landing/pages/faq.tsx
@@ -45,7 +45,7 @@ const FaqPage: React.FC = () => {
           description: "Frequently asked questions.",
           images: [
             {
-              url: "https://floatingfile.space/images/landing-banner.png",
+              url: "https://floatingfile.space/images/banner-blue-1200x600.jpg",
               width: 1200,
               height: 630,
               alt: "Use floatingfile today!",
