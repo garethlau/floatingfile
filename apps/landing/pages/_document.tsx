13b0e449a7b2e3039aabc2591723c8b1f2c64581
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-01-17 09:30:36 -0700

    Update gtag script

--- apps/landing/pages/_document.tsx
+++ apps/landing/pages/_document.tsx
@@ -92,14 +92,18 @@ class MyDocument extends Document {
                 src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
                 async
               />
-              <script>
-                {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', ${GA_TRACKING_ID});
-                `}
-              </script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
             </>
           )}
         </Head>
