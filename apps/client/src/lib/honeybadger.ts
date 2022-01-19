e6aed2b9977942e7529bdc96f363f4ab60b1204c
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-01-18 20:43:21 -0700

    Update honeybadger config

--- apps/client/src/lib/honeybadger.ts
+++ apps/client/src/lib/honeybadger.ts
@@ -1,13 +1,10 @@
 import Honeybadger from "@honeybadger-io/js";
 
 Honeybadger.configure({
-  apiKey: process.env.REACT_APP_HONEYBADGER_API_KEY || "",
+  apiKey: process.env.REACT_APP_HONEYBADGER_API_KEY,
   environment: process.env.REACT_APP_ENVIRONMENT,
-  disabled: process.env.REACT_APP_ENVIRONMENT === "development",
+  reportData: process.env.REACT_APP_ENVIRONMENT === "production",
   breadcrumbsEnabled: {
-    dom: true,
-    network: true,
-    navigation: true,
     console: false,
   },
 });
