dcb0bd95f03bb6af1028d8535821dc71b118eabd
Gareth <gareth.kh.lau@gmail.com>
2022-05-15 06:07:56 +0000

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@ die () {
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 branch=$1 
 
-peralta = "peralta_${branch}"
-judy = "judy_${branch}"
+peralta="peralta_${branch}"
+judy="judy_${branch}"
 
 
 echo ">>> deploying floatingfile"
@@ -27,18 +27,18 @@ yarn --cwd packages/ui build
 yarn --cwd packages/types build
 
 echo ">>> building frontend"
-if [ "$branch" = "master" ]; then 
-    yarn --cwd app/client build:master
-else 
-    yarn --cwd apps/client build:staging
-fi
+# if [ "$branch" = "master" ]; then 
+    # yarn --cwd app/client build:master
+# else 
+   #  yarn --cwd apps/client build:staging
+# fi
 
 echo ">>> building backend"
-yarn --cwd apps/server build
+# yarn --cwd apps/server build
 
 echo ">>> restarting application"
 if [ "$(pm2 id ${peralta})" = "[]" ]; then 
-    pm2 start npm --name $perlata --start 
+    pm2 start npm --name $perlata -- start:server
 else 
     pm2 reload $peralta 
 fi
@@ -50,7 +50,7 @@ yarn --cwd apps/landing build
 
 echo ">>> starting landing server"
 if [ "$(pm2 id ${judy})" = "[]" ]; then
-    pm2 start npm --name $judy -- start # start next app
+    pm2 start npm --name $judy -- start:landing
 else
     pm2 reload $judy
 fi
