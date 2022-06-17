e9ccde77db797d76b0e971326f3dbe62ee3c6637
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-06-17 00:26:01 -0400

    Add transport to datadog logger in production

--- apps/server/src/lib/logger.ts
+++ apps/server/src/lib/logger.ts
@@ -1,6 +1,8 @@
 import logger, { format, transports, createLogger } from "winston";
-import { NODE_ENV } from "../config";
+import { NODE_ENV, DD_API_KEY } from "../config";
 const { combine, timestamp, json, colorize, simple, printf } = format;
+
+const SERVICE_NAME = "server";
 
 logger.add(
   new transports.File({
@@ -9,11 +11,24 @@ logger.add(
     format: combine(timestamp(), json()),
   })
 );
+
 if (NODE_ENV !== "production") {
   logger.add(
     new transports.Console({
       format: combine(colorize(), simple()),
       level: "info",
+    })
+  );
+}
+
+if (NODE_ENV === "production") {
+  logger.add(
+    new transports.Http({
+      host: "http-intake.logs.datadoghq.com",
+      path: `/api/v2/logs?dd-api-key=${DD_API_KEY}&ddsource=nodejs&service=${SERVICE_NAME}`,
+      format: format.json(),
+      level: "info",
+      ssl: true,
     })
   );
 }
@@ -27,7 +42,6 @@ export const accessLogger = createLogger({
         printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
       ),
     }),
-
     new transports.File({
       filename: `${process.cwd()}/logs/access.log`,
       level: "info",
