5443779e2bf138beea121fba5b99b4d3d5e9962e
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-12-15 03:53:18 -0500

    Handle error

--- apps/server/src/router/tunnels-router.ts
+++ apps/server/src/router/tunnels-router.ts
@@ -1,22 +1,37 @@
 import { Router } from "express";
 import { generateCode } from "../services/code-service";
 import redis from "../lib/redis";
+import logger from "../lib/logger";
 
 const router = Router();
 
 router.get("/:code", async (req, res) => {
-  const code = req.params.code as string;
-  const url = await redis.get(code);
-  return res.status(200).send({ url });
+  try {
+    const code = req.params.code as string;
+    const url = await redis.get(code);
+    return res.status(200).send({ url });
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(error.stack);
+    }
+    return res.status(505).send();
+  }
 });
 
 router.post("/", async (req, res) => {
-  const { url } = req.body as Record<string, string>;
-  const code = await generateCode();
+  try {
+    const { url } = req.body as Record<string, string>;
+    const code = await generateCode();
 
-  await redis.set(code, url, { EX: 6 * 60 * 60 });
+    await redis.set(code, url, { EX: 6 * 60 * 60 });
 
-  return res.status(200).send({ code });
+    return res.status(200).send({ code });
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(error.stack);
+    }
+    return res.status(505).send();
+  }
 });
 
 export default router;
