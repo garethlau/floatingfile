3d863781c1c42ae9355efde961b14eb54e257148
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-12-15 02:31:36 -0500

    Register tunnels endpoints

--- apps/server/src/router/index.ts
+++ apps/server/src/router/index.ts
@@ -3,6 +3,7 @@ import notificationsRouter from "./notifications-router";
 import zipRouter from "./zip-router";
 import filesRouter from "./files-router";
 import spacesRouter from "./spaces-router";
+import tunnelsRouter from "./tunnels-router";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/zip", zipRouter);
 router.use("/notifications", notificationsRouter);
 router.use("/files", filesRouter);
 router.use("/spaces", spacesRouter);
+router.use("/tunnels", tunnelsRouter);
 
 export default router;
