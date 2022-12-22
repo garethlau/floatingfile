5bbc53e9311268b071f43d21d88189197b3028d7
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-12-22 13:18:22 -0700

    Remove stats collector

--- apps/server/src/crons/index.ts
+++ apps/server/src/crons/index.ts
@@ -1,13 +1,8 @@
 import cron from "node-cron";
 import { expireSpaces } from "./expire-spaces";
-import { getOsStats } from "./get-os-stats";
 
 export function scheduleJobs() {
   cron.schedule("* * * * *", () => {
     expireSpaces();
-  });
-
-  cron.schedule("* * * * *", () => {
-    getOsStats();
   });
 }
