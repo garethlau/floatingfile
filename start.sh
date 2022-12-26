b7e2a00722eef2a8452bc3508e51adda40cc785b
Gareth Lau <Gareth.kh.lau@gmail.com>
2022-12-26 13:02:40 -0700

    Remove Nextjs start command from start script

--- start.sh
+++ start.sh
@@ -2,7 +2,5 @@
 PATH="/root/.nvm/versions/node/v14.19.1/bin:$PATH"
 
 yarn --cwd ./apps/server start & PIDSERVER=$!
-yarn --cwd ./apps/landing start & PIDLANDING=$!
 
 wait $PIDSERVER
-wait $PIDLANDING
